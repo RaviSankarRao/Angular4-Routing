3a99b1c5d0535ed626098bff75a94f68273f98de
RaviSankarRao <ravisankar139@gmail.com>
2018-02-20 23:34:06 -0800

    Child/Nested routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,22 +16,40 @@ export const appRoutes: Routes = [
   },
   {
     path: 'users',
-    component: UsersComponent
+    component: UsersComponent,
+    children: [
+      {
+        path: ':id/:name',
+        component: UserComponent
+      },
+    ]
   },
-  {
-    path: 'users/:id/:name', //dynamic parameter 'id'. This user's id can be retirved from the route using ActivatedRoute
-    component: UserComponent
-  },
+  // {
+  //   path: 'users/:id/:name', //dynamic parameter 'id'. This user's id can be retirved from the route using ActivatedRoute
+  //   component: UserComponent
+  // },
   {
     path: 'servers',
-    component: ServersComponent
+    component: ServersComponent,
+    children: [
+      {
+        path: ':id',
+        component: ServerComponent
+      },
+      {
+        path: ':id/edit',
+        component: EditServerComponent
+      }
+    ]
   },
-  {
-    path: 'servers/:id',
-    component: ServerComponent
-  },
-  {
-    path: 'servers/:id/edit',
-    component: EditServerComponent
-  }
+  // Below routes are moved as child/nested routes, as shown above
+
+  // {
+  //   path: 'servers/:id',
+  //   component: ServerComponent
+  // },
+  // {
+  //   path: 'servers/:id/edit',
+  //   component: EditServerComponent
+  // }
 ];
